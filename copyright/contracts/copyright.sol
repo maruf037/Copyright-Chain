4677fd76b62586796bad041facd66c3583f935ee
Md. Maruf Hossain <69377574+md-maruf-hossain@users.noreply.github.com>
2021-12-30 11:08:50 +0600

    second commit

--- copyright/contracts/copyright.sol
+++ copyright/contracts/copyright.sol
@@ -21,6 +21,28 @@ contract Copyright {
 
     uint256 public counter = 0;
     address payable public owner;
+
+    //To setup the owner of the contract
+    constructor() public{
+        owner = msg.sender;
+    }
+
+    function addContent(bytes32 _hashId, string memory _contentUrl,
+    string memory _email, string memory _termsOfUse) public {
+        
+        //Check that the most important values are not empty
+        require(_hashId != 0 && bytes(_contentUrl).length != 0
+        && bytes(_email).length != 0);
+
+        counter += 1;
+        Content memory newContent = Content(counter, _hashId,
+        _contentUrl, msg.sender, now, email, _termsOfUse);
+        
+        copyrightsById(_hashId) = newContent;
+        
+        emit RegisteredContent(counter, _hashId, _contentUrl,
+        msg.sender, now, _email, _termsOfUse);
+    }
 }
 
 
